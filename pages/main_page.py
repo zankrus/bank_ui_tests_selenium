54c2b7c8c6a2ce5612ee788dd70c4a65e7368457
Ro <sealthepirate@gmail.com>
2020-08-13 18:31:12 +0300

    add active waiting

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,6 +2,8 @@ from typing import Any
 
 import allure
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from locators.main_page import MainPageLocators
 
@@ -9,12 +11,14 @@ from locators.main_page import MainPageLocators
 class MainPage:
     def __init__(self, app):
         self.app = app
+        self.wait = WebDriverWait(self.app.wd, 10)
 
     def deposits_button(self) -> WebElement:
         return self.app.wd.find_element(*MainPageLocators.deposits)
 
     @allure.step("Нажатие на кнопку Вклады")
     def click_on_deposits(self) -> Any:
+        self.wait.until(EC.element_to_be_clickable(MainPageLocators.deposits))
         return self.deposits_button().click()
 
     def change_lang_button(self) -> WebElement:
@@ -29,6 +33,7 @@ class MainPage:
 
     @allure.step("Нажать на кнопку Карты")
     def click_on_cards_button(self) -> Any:
+        self.wait.until(EC.element_to_be_clickable(MainPageLocators.cards))
         return self.cards_button().click()
 
     def logout_button(self) -> WebElement:
